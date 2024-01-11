d03d16519b870377061ce9fb196c4179a9b8cda5
Ashish Sinha <ashishsinha108@gmail.com>
2024-01-10 20:35:46 -0800

    updates toml

--- dermsynth3d/__init__.py
+++ dermsynth3d/__init__.py
@@ -1,7 +1,2 @@
-from .datasets import *
-from .deepblend import *
 from .version import __version__
-from .models import *
-from .utils import *
 from .tools import *
-from .losses import *
--- dermsynth3d/version.py
+++ dermsynth3d/version.py
@@ -1,1 +1,15 @@
 __version__ = "0.1.0"
+#
+# pytorch3d = [
+#     {url = "https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu113_pyt1121/pytorch3d-0.7.2-cp38-cp38-linux_x86_64.whl"}
+#     {url = "https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py39_cu113_pyt1121/pytorch3d-0.7.2-cp39-cp39-linux_x86_64.whl"}
+#     {url = "https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py310_cu113_pyt1121/pytorch3d-0.7.2-cp10-cp310-linux_x86_64.whl"}
+#     {url = "https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu116_pyt1121/pytorch3d-0.7.2-cp38-cp38-linux_x86_64.whl"}
+#     {url = "https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py39_cu116_pyt1121/pytorch3d-0.7.2-cp39-cp39-linux_x86_64.whl"}
+#     {url = "https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py310_cu116_pyt1121/pytorch3d-0.7.2-cp310-cp310-linux_x86_64.whl"}
+# ]
+#
+# pytorch3d = { git = "https://github.com/facebookresearch/pytorch3d.git", tag = "stable", extras = ["torch"], python = "^3.8", torch = "^1.12", platform = "linux" }
+# pytorch3d = {url = "https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py38_cu113_pyt1121/download.html"}
+#
+# pytorch3d = { git = "https://github.com/facebookresearch/pytorch3d.git", tag = "stable", extras = ["torch"]}
